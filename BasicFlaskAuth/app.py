77f41c49c23ec95bde2e33e4b760bdcba2722879
Ravi <ravinder9.reddy@gmail.com>
2020-02-09 14:04:39 +0000

    enabled auth0

--- BasicFlaskAuth/app.py
+++ BasicFlaskAuth/app.py
@@ -4,12 +4,11 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
 app = Flask(__name__)
 
-AUTH0_DOMAIN = @TODO_REPLACE_WITH_YOUR_DOMAIN
+AUTH0_DOMAIN = 'ravi-fsnd.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = @TODO_REPLACE_WITH_YOUR_API_AUDIENCE
+API_AUDIENCE = 'image'
 
 
 class AuthError(Exception):
@@ -100,25 +99,49 @@ def verify_decode_jwt(token):
                 'description': 'Unable to parse authentication token.'
             }, 400)
     raise AuthError({
-                'code': 'invalid_header',
-                'description': 'Unable to find the appropriate key.'
-            }, 400)
+        'code': 'invalid_header',
+        'description': 'Unable to find the appropriate key.'
+    }, 400)
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        token = get_token_auth_header()
-        try:
-            payload = verify_decode_jwt(token)
-        except:
-            abort(401)
-        return f(payload, *args, **kwargs)
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        abort(400)
 
-    return wrapper
+    if permission not in payload['permission']:
+        abort(403)
+
+    return True
+
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            token = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(token)
+            except:
+                abort(401)
+
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+
+        return wrapper
+
+    return requires_auth_decorator
+
 
 @app.route('/headers')
 @requires_auth
 def headers(payload):
     print(payload)
+    return 'Access Granted'
+
+
+@app.route('/images')
+@requires_auth('get:images')
+def images(jwt):
+    print(jwt)
     return 'Access Granted'
