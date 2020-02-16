54b844a917f4ae2839c046284eddfd5a87a8de3f
Ravi <ravinder9.reddy@gmail.com>
2020-02-16 13:21:56 +0000

    feature is in progress

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/auth/auth.py
@@ -4,16 +4,11 @@ from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
-
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'ravi-fsnd.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'Coffee shop'
 
-## AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
@@ -41,7 +36,7 @@ def get_token_auth_header():
             'code': 'invalid_header',
             'description': 'token not found.'
         }, 401)
-    elif len(parts)> 2:
+    elif len(parts) > 2:
         raise AuthError({
             'code': 'invalid_header',
             'description': 'authorization must be bearer token.'
@@ -58,24 +53,12 @@ def check_permissions(permission, payload):
         abort(403)
     return True
 
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
 
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
-    jwks =json.loads(jsonurl.read())
-    unverified_header =jwt.get_unverified_header(token)
-    rsa_key ={}
+    jwks = json.loads(jsonurl.read())
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
     if 'kid' not in unverified_header:
         raise AuthError({
             'code': 'invalid header',
@@ -124,16 +107,7 @@ def verify_decode_jwt(token):
         'description': 'unable to find appropriate key'
     }, 400)
 
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
@@ -148,4 +122,5 @@ def requires_auth(permission=''):
             return f(payload, *args, **kwargs)
 
         return wrapper
+
     return requires_auth_decorator
